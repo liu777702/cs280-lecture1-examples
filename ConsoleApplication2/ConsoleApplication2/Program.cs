f44ab7e716b81eb3347541faaacd3fc77e1b0f44
liu777702 <liu777702@gmail.com>
2017-03-03 22:19:10 +0800

    新增Pokemon類別

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Pokemon pokemon = new Pokemon();
+            pokemon.Height = 2.0f;
+            pokemon.Weight = 12.7f;
+            pokemon.Category = "Lizard";
+            pokemon.Abilities = "Blaze";
+            pokemon.Gender = "M";
+            Console.WriteLine("身高:{0},體重{1}",
+                pokemon.Height, pokemon.Weight);
+        }
+    }
+}
